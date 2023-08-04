40e6a769d46de25a229de1a30e6ee0ca2bb487a0
Isendoum <geotsek2003@yahoo.gr>
2023-08-04 20:09:52 +0300

    Added submenus in nav menu

--- src/components/core/navMenu/NavMenuItem.tsx
+++ src/components/core/navMenu/NavMenuItem.tsx
@@ -10,33 +10,51 @@ const NavMenuItem = ({
   icon,
   setIsClosing,
   children,
+  expandedMenu, // receive the state
+  setExpandedMenu, // receive the setter
 }: {
   title: string;
   link: string;
   icon?: any;
+  expandedMenu: any;
+  setExpandedMenu: (val: any) => void | undefined;
   setIsClosing?: (val: boolean) => void;
   children?: any[];
 }) => {
   const router = useRouter();
+  const isExpanded = expandedMenu === title;
   const handleClick = () => {
     setIsClosing ? setIsClosing(true) : null;
     router.push(link);
   };
+
+  const handleExpandClick = () => {
+    // if this menu is already expanded, collapse it, otherwise expand it
+    setExpandedMenu(isExpanded ? null : title);
+  };
+
   return (
     <li id={title} className={`flex flex-col w-full`}>
-      <div className={`flex flex-row items-center`}>
+      <div className={`flex flex-row items-center relative`}>
         <div className="flex flex-row w-full justify-between p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white">
           <div onClick={handleClick} className="flex">
             <MenuItemTitle title={title} />
           </div>
           {children && (
-            <div className="flex self-end cursor-pointer">
-              <ChevronDownIcon className={`w-6 h-6`} />
+            <div className="flex self-end ml-2 cursor-pointer">
+              <ChevronDownIcon
+                onClick={handleExpandClick} // toggle expansion here
+                className={`w-6 h-6`}
+              />
             </div>
           )}
         </div>
+        <div className="absolute -bottom-[5.7rem] flex flex-col">
+          <div className="bg-[#000080] rounded-b-lg">
+            {isExpanded && children}
+          </div>
+        </div>
       </div>
-      {children}
     </li>
   );
 };
--- src/components/core/navMenu/index.tsx
+++ src/components/core/navMenu/index.tsx
@@ -2,7 +2,15 @@
 import Link from "next/link";
 import NavMobileMenu from "./NavMobileMenu";
 import NavMenuItem from "./NavMenuItem";
-import { useCallback, useState } from "react";
+import {
+  DetailedHTMLProps,
+  HTMLAttributes,
+  LegacyRef,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
@@ -11,31 +19,62 @@ const menu = [
   {
     label: "Home",
     link: "/",
+    children: [
+      {
+        label: "Other",
+        link: "/1",
+      },
+      {
+        label: "Other 2 Other 2 Other 2",
+        link: "/2",
+      },
+    ],
+  },
+  {
+    label: "Services",
+    link: "/3",
+    children: [
+      {
+        label: "Service 1",
+        link: "/4",
+      },
+      {
+        label: "Service 2",
+        link: "/5",
+      },
+    ],
   },
 ];
 
 const NavMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isClosing, setIsClosing] = useState(false);
+  const [expandedMenu, setExpandedMenu] = useState<String | null>(null);
   const session = useSession();
   const pathname = usePathname();
+  const navMenuRef = useRef<HTMLDivElement>(null);
   const setIsOpenCallback = useCallback((val: boolean) => {
     setIsOpen(val);
   }, []);
   const setIsClosingCallback = useCallback((val: boolean) => {
     setIsClosing(val);
   }, []);
-
+  const setIsExpandedCallback = useCallback((val: string) => {
+    setExpandedMenu(val);
+  }, []);
   const renderMenuItems = (
     menuItems: any,
-    setIsOpen?: (val: boolean) => void,
-    setIsClosing?: (val: boolean) => void
+    setIsClosing?: (val: boolean) => void,
+    expandedMenu?: any,
+    setExpandedMenu?: any
   ) => {
     return menuItems.map((mItem: any) => (
       <NavMenuItem
         key={mItem.link}
         title={mItem.label}
         link={mItem.link}
+        expandedMenu={expandedMenu} // pass down the state
+        setExpandedMenu={setExpandedMenu} // pass down the setter
         setIsClosing={setIsClosing}>
         {mItem.children && (
           <ul className="pl-2">{renderMenuItems(mItem.children)}</ul>
@@ -43,13 +82,37 @@ const NavMenu: React.FC = () => {
       </NavMenuItem>
     ));
   };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      //@ts-ignore
+      if (navMenuRef.current && !navMenuRef.current.contains(event.target)) {
+        setExpandedMenu(null);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Clean up the event listener on unmount
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   if (pathname.includes("/platform")) {
     return null;
   }
   return (
-    <div className="w-full">
-      <div className="flex flex-row w-full bg-black h-[4rem] justify-between items-center">
-        <div>{renderMenuItems(menu)}</div>
+    <div className="w-full" ref={navMenuRef}>
+      <div className="flex flex-row w-full bg-[#4169E1] h-[4rem] justify-between items-center">
+        <div className="max-lg:hidden flex flex-row pl-6 items-center">
+          <p className="pr-6">Logo</p>
+          {renderMenuItems(
+            menu,
+            () => null,
+            expandedMenu,
+            setIsExpandedCallback
+          )}
+        </div>
         {session?.status !== "authenticated" && (
           <div className="flex flex-row items-center mr-4">
             <Link
