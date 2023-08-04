2d540463b4d978aec631d59a10a2de48adfaabd9
Isendoum <geotsek2003@yahoo.gr>
2023-08-04 12:09:45 +0300

    Fixed text bug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
           <Link
             className="inline-block mt-4 self-center px-6 py-3 text-white bg-blue-500 rounded-lg shadow-lg hover:bg-blue-600"
             href="/signUp">
-            <span className="text-1xl ">Don't have an account? Sign up!</span>
+            <span className="text-1xl ">{`Don't have an account? Sign up!`}</span>
           </Link>
         </div>
       </SimpleSpace>
