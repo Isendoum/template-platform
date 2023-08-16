2aefe5db98f6f87eaa1b20316d97ee90216a02e0
Isendoum <geotsek2003@yahoo.gr>
2023-08-16 17:13:24 +0300

    Added proper close icon in mobile menus

--- src/components/core/drawerMenu/MobileMenu.tsx
+++ src/components/core/drawerMenu/MobileMenu.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-
+import { XMarkIcon } from "@heroicons/react/24/solid";
 const MobileMenu = ({
   isOpen,
   setIsOpen,
@@ -22,25 +22,11 @@ const MobileMenu = ({
           <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
             <div className="pointer-events-auto relative w-screen max-w-md">
               <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
-                <button
+                <span className="sr-only">Close panel</span>
+                <XMarkIcon
+                  className="w-6 h-6 cursor-pointer text-gray-300 hover:text-white lg:hidden mr-4"
                   onClick={() => setIsOpen(false)}
-                  type="button"
-                  className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white">
-                  <span className="sr-only">Close panel</span>
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
+                />
               </div>
 
               <div className="flex h-full flex-col overflow-y-hidden bg-white py-6 shadow-xl bg-gray-50 dark:bg-gray-800">
--- src/components/core/navMenu/NavMobileMenu.tsx
+++ src/components/core/navMenu/NavMobileMenu.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
 const NavMobileMenu = ({
@@ -38,25 +39,12 @@ const NavMobileMenu = ({
           <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
             <div className="pointer-events-auto relative w-screen max-w-md">
               <div className="absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4">
-                <button
+                <XMarkIcon
+                  className="w-6 h-6 cursor-pointer text-gray-300 hover:text-white lg:hidden mr-4"
                   onClick={() => setIsClosing(true)}
-                  type="button"
-                  className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white">
-                  <span className="sr-only">Close panel</span>
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true">
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
+                />
+
+                <span className="sr-only">Close panel</span>
               </div>
 
               <nav className="flex h-full flex-col overflow-y-hidden py-6 shadow-xl">
