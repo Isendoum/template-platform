bb6effa08ff2b74e02ee6fc49186fc58ccda4c63
Isendoum <geotsek2003@yahoo.gr>
2023-12-06 17:57:48 +0200

    test pre-commit hook 5

--- src/app/platform/dashboard/page.tsx
+++ src/app/platform/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { useCallback, useEffect, useState } from "react";
 
 const Dashboard = () => {
   const [data, setData] = useState<any>([]);
-
+  const a ="";
   const fetchData = useCallback(() => {
     const data1 = [
       { x: "2023-04-15", y: 6 },
